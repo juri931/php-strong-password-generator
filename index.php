d57367ad9e5cf5c6ef7783384d32d647a754ac6b
juri931 <impa.mat@hotmail.it>
2024-04-16 17:28:32 +0200

    [fix] some fixes

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+  $error = '';
+
 ?>
+
+<?php include_once __DIR__ . '../data/functions.php';?>
+
 
 <?php include_once __DIR__ . './partials/head.php';?>
 
@@ -14,7 +19,11 @@
       <tbody>
         <tr>
           <td>Lunghezza password:</td>
-          <td><input type="text"></td>
+          <td>
+            <form action="index.php" method="GET">
+              <input type="text" class="form-control" id="number" name="number">
+            </form>
+          </td>
         </tr>
         
         <tr>
@@ -44,7 +53,7 @@
 
         <tr>
           <td>
-            <button class="btn btn-primary">Invia</button>
+            <button type="submit" class="btn btn-primary">Invia</button>
             <button class="btn btn-secondary">Annulla</button>
           </td>
         </tr>
